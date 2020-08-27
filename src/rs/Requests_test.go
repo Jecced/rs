e3e71331a492ce017bbc2bbe26b03c87c06d273b
Jecced <jecced@gmail.com>
2020-08-27 17:08:43 +0800

    实现Get(url).Send().WriteToFile(path)

--- src/rs/Requests_test.go
+++ src/rs/Requests_test.go
@@ -9,3 +9,9 @@ func Test01(t *testing.T) {
 	resp := Get("http://www.baidu.com/img/sug_bd.png?v=09816787.png").Send().ReadText()
 	fmt.Println(resp)
 }
+
+func Test02(t *testing.T) {
+	Get("http://www.baidu.com/img/sug_bd.png?v=09816787.png").
+		Send().
+		WriteToFile("/Users/ankang/develop/test/test/1.png")
+}
