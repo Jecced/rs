4e239cc2cd6a2d87524d222f0b1be807077040fa
Jecced <jecced@gmail.com>
2020-08-28 19:15:06 +0800

    rename test func method name

--- src/rs/Requests_test.go
+++ src/rs/Requests_test.go
@@ -5,18 +5,18 @@ import (
 	"testing"
 )
 
-func Test01(t *testing.T) {
-	resp := Get("http://www.baidu.com/img/sug_bd.png?v=09816787.png").Send().ReadText()
+func TestGetReadText(t *testing.T) {
+	resp := Get("http://www.baidu.com/").Send().ReadText()
 	fmt.Println(resp)
 }
 
-func Test02(t *testing.T) {
+func TestGetWriteToFile(t *testing.T) {
 	Get("http://www.baidu.com/img/sug_bd.png?v=09816787.png").
 		Send().
 		WriteToFile("/Users/ankang/develop/test/test/1.png")
 }
 
-func Test03(t *testing.T) {
+func TestGetProxy(t *testing.T) {
 	resp := Get("http://www.google.com").Proxy("127.0.0.1:1081").Send().ReadText()
 	fmt.Println(resp)
 }
