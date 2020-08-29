797d568642e644f3e9b2f2fb3ab40d3753782a08
Jecced <jecced@gmail.com>
2020-08-29 22:49:35 +0800

    gen get url format

--- src/rs/Get.go
+++ src/rs/Get.go
@@ -1,11 +1,34 @@
 package rs
 
-import "fmt"
+import (
+	"strings"
+)
 
-func Print() {
-	fmt.Println("Test Print, Welcome Use RS Libs")
-}
+// 生成get请求的url
+func (r *Request) getGetUrl() string {
+	uri := r.uri
+	param := r.param
 
-func Test() {
-	fmt.Println("Test func")
+	// 参数列表为空 直接返回
+	if 0 == len(param) {
+		return uri
+	}
+
+	body := param.Encode()
+
+	markWithEnd := strings.HasSuffix(uri, "?")
+
+	// 问号结尾, 直接返回uri + param
+	if markWithEnd {
+		return uri + body
+	}
+
+	hasMark := strings.Index(uri, "?") != -1
+	// 有问号, 但是在中间
+	if hasMark {
+		return uri + "&" + body
+	}
+
+	// 没有问号
+	return uri + "?" + body
 }
