4f33d6094944957eb382a7982e01763e2952c137
Jecced <jecced@gmail.com>
2020-08-31 10:57:46 +0800

    add set head timeout

--- src/rs/Timeout.go
+++ src/rs/Timeout.go
@@ -18,3 +18,9 @@ func (r *Request) SetRespTimeOut(time int) *Request {
 	r.respTimeout = time
 	return r
 }
+
+// 设置请求头的超时时间
+func (r *Request) SetHeadTimeOut(time int) *Request {
+	r.headTimeout = time
+	return r
+}
