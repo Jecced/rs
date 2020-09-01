f7e21c379a2977e0431fabae09a8ae1608eeb6cb
Jecced <jecced@gmail.com>
2020-09-01 15:40:24 +0800

    remove deprecated method, use SetRespTimeOut replace SetHeadTimeOut method

--- src/rs/Timeout.go
+++ src/rs/Timeout.go
@@ -18,12 +18,3 @@ func (r *Request) SetRespTimeOut(time int) *Request {
 	r.respTimeout = time
 	return r
 }
-
-// 设置请求头的超时时间
-// 已废弃: 请使用 SetRespTimeOut 来替代 SetHeadTimeOut 方法
-// Deprecated: use SetRespTimeOut replace SetHeadTimeOut
-func (r *Request) SetHeadTimeOut(time int) *Request {
-	//r.headTimeout = time
-	r.respTimeout = time
-	return r
-}
