f25821d0569a79db0c7b0035397350752e87c2df
Jecced <jecced@gmail.com>
2020-09-01 15:18:14 +0800

    fix post param head

--- src/rs/Header.go
+++ src/rs/Header.go
@@ -9,4 +9,8 @@ func (r *Request) setHeader(request *http.Request) {
 	if r.auth != nil {
 		request.Header.Set("Authorization", r.auth.encode())
 	}
+
+	if r.reqType == POST && len(r.param) > 0 {
+		request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	}
 }
--- src/rs/Post.go
+++ src/rs/Post.go
@@ -1,13 +1,11 @@
 package rs
 
 import (
-	"encoding/json"
 	"net/http"
 	"strings"
 )
 
-// get请求将生成get请求url
+// 生成post请求
 func (r *Request) postNewRequest() (*http.Request, error) {
-	info, _ := json.Marshal(r.param)
-	return http.NewRequest(r.reqType, r.uri, strings.NewReader(string(info)))
+	return http.NewRequest(r.reqType, r.uri, strings.NewReader(r.param.Encode()))
 }
