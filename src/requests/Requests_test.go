e458a2efc01a5a7c022177862d55ba7ebb124674
Jecced <jecced@gmail.com>
2020-09-03 15:03:49 +0800

    update test

---
+++ src/requests/Requests_test.go
@@ -0,0 +1,23 @@
+package requests
+
+import (
+	"fmt"
+	"github.com/Jecced/rs/src/requests/rs"
+	"testing"
+)
+
+func TestGetReadText(t *testing.T) {
+	resp := rs.Get("http://www.baidu.com/").Send().ReadText()
+	fmt.Println(resp)
+}
+
+func TestGetWriteToFile(t *testing.T) {
+	rs.Get("http://www.baidu.com/img/sug_bd.png?v=09816787.png").
+		Send().
+		WriteToFile("/Users/ankang/develop/test/test/1.png")
+}
+
+func TestGetProxy(t *testing.T) {
+	resp := rs.Get("http://www.google.com").Proxy("127.0.0.1:1081").Send().ReadText()
+	fmt.Println(resp)
+}
